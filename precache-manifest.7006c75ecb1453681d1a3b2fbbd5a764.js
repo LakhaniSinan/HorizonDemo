989b4aab1e9646ce6c4366232d6ac0a9d0065ef4
sinan <sinan.lakhani@hotmail.com>
2020-06-24 01:56:59 -0700

    Updates

---
+++ precache-manifest.7006c75ecb1453681d1a3b2fbbd5a764.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b23d5b9c3116ec2830a25093c3afe4",
+    "url": "/HorizonDemo/index.html"
+  },
+  {
+    "revision": "1b93364747c0eb083206",
+    "url": "/HorizonDemo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d7ca7b129e027dadbf9b",
+    "url": "/HorizonDemo/static/css/main.61b28c0f.chunk.css"
+  },
+  {
+    "revision": "1b93364747c0eb083206",
+    "url": "/HorizonDemo/static/js/2.8fd5af51.chunk.js"
+  },
+  {
+    "revision": "df835edbe6947b41c9ad692616389269",
+    "url": "/HorizonDemo/static/js/2.8fd5af51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ca7b129e027dadbf9b",
+    "url": "/HorizonDemo/static/js/main.f360ae66.chunk.js"
+  },
+  {
+    "revision": "76795bedea99d7378231",
+    "url": "/HorizonDemo/static/js/runtime-main.4d522a8f.js"
+  },
+  {
+    "revision": "d44ee16c4c56b31517333e0d9b6293c1",
+    "url": "/HorizonDemo/static/media/ihorizon.d44ee16c.png"
+  },
+  {
+    "revision": "9305a29551b93a7aea398611aa88c5e6",
+    "url": "/HorizonDemo/static/media/logo1.9305a295.png"
+  },
+  {
+    "revision": "9042f8b7abc6afd0bb3702c817541030",
+    "url": "/HorizonDemo/static/media/logo2.9042f8b7.png"
+  },
+  {
+    "revision": "66a2fd0440dbebeabf47999e032a241a",
+    "url": "/HorizonDemo/static/media/logo3.66a2fd04.png"
+  },
+  {
+    "revision": "ffcf8cae240c8e928ebc75c4055bbacf",
+    "url": "/HorizonDemo/static/media/logo4.ffcf8cae.png"
+  }
+]);
